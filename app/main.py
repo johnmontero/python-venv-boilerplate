0e7ea7967c6de795847ee0f72eeb9bfb93338af9
John Montero <jmonteroc@gmail.com>
2018-05-29 12:14:55 -0500

    agregar impresion de logs por consola

--- app/main.py
+++ app/main.py
@@ -4,16 +4,18 @@ from __future__ import print_function
 import os
 import sys
 import yaml
-import boto3
-import re
 import logging
 
 logger = logging.getLogger(__name__)
-
+logging.basicConfig(
+    stream=sys.stdout, 
+    level=logging.INFO,
+    format='[%(asctime)s] %(levelname)s - %(message)s'
+)
 
 def main():
     try:
-        logger.info('Process...')
+        logger.info('Procesando main.py...')
     except exceptions.ExceptionHandler as error:
         logger.error(str(error), exc_info=True)
         
